a1704e19f5cfb4c6ee11903b063e3e96b128f006
Riccardo Mancini <r.mancini@santannapisa.it>
2019-10-12 18:41:47 +0200

    Added Request classes to map to incoming JSON requests

---
+++ Java_server/src/main/java/AddMemberRequest.java
@@ -0,0 +1,11 @@
+public class AddMemberRequest {
+    private long userId;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
---
+++ Java_server/src/main/java/CreateChatRequest.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class CreateChatRequest {
+    private String name;
+    private List<Long> members;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Long> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Long> members) {
+        this.members = members;
+    }
+}
