58c3fd7d7728b9d82c5be9b889601992c7de8987
Mirko <mirkoletto@gmail.com>
2019-10-13 21:57:32 +0200

    Login/signout page + small bugfix

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Button, Form, Alert } from "react-bootstrap";
+import {Link, Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -35,7 +36,18 @@ class LoginForm extends Component {
   handleSubmit(evt){
     evt.preventDefault();
     var self = this;
-    axios.post('http://'+window.location.hostname+':8080/api/v1/auth/login', {
+    var postUrl;
+    this.setState({
+      wrongAuth: 'hidden',
+    }) 
+    var path = window.location.href.split('/')[3]
+    if(path !== 'register'){
+      postUrl = 'http://'+window.location.hostname+':8080/api/v1/auth/login';
+    } else {
+      postUrl = 'http://'+window.location.hostname+':8080/api/v1/users';
+    }
+
+    axios.post(postUrl, {
       username: this.state.username,
       password: this.state.password
     })
@@ -61,26 +73,47 @@ class LoginForm extends Component {
   render() {
     return (
       <div className="Login">
-        <Form onSubmit={this.handleSubmit}>
-          <h2>EasyChat</h2>
-          <Form.Group controlId="formUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text"placeholder="Enter username"
-                          value={this.state.username} onChange={(evt) => this.handleUser(evt.target.value)}/>
-          </Form.Group>
+        <Router>
+          <Form onSubmit={(evt) => this.handleSubmit(evt, this.props)}>
+            <h2>EasyChat</h2>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text"placeholder="Enter username"
+                            value={this.state.username} onChange={(evt) => this.handleUser(evt.target.value)}/>
+            </Form.Group>
 
-          <Form.Group controlId="formPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password"
-                          value={this.state.password} onChange={(evt) => this.handlePw(evt.target.value)} />
-          </Form.Group>
-          <Alert variant="danger"
-                style={{ marginBottom: '20px'}}
-                className={this.state.wrongAuth ? 'hidden' : ''}>Wrong username or password</Alert>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password"
+                            value={this.state.password} onChange={(evt) => this.handlePw(evt.target.value)} />
+            </Form.Group>
+
+            <Switch>
+              <Route path="/register">
+                <Alert variant="danger"
+                  style={{ marginBottom: '20px'}}
+                  className={this.state.wrongAuth ? 'hidden' : ''}>Username already taken</Alert>
+                <Link to="/">
+                  <Button variant="link">Log in</Button>
+                </Link>
+                <Button variant="primary" type="submit">
+                  Sign on!
+                </Button>
+              </Route>
+              <Route path="/">
+                <Alert variant="danger"
+                  style={{ marginBottom: '20px'}}
+                  className={this.state.wrongAuth ? 'hidden' : ''}>Wrong username or password</Alert>
+                <Link to="/register">
+                  <Button variant="link">Sign on!</Button>
+                </Link>
+                <Button variant="primary" type="submit">
+                  Log in!
+                </Button>
+              </Route>
+            </Switch>
+          </Form>
+        </Router>
       </div>
     );
   }
