69882f4f3690b82ec068f6f4cba5bd278b4a0383
Mirko <mirkoletto@gmail.com>
2019-10-15 18:41:33 +0200

    Cleaning working directory

--- Java_server/src/main/java/BooleanResult.java
+++
@@ -1,7 +0,0 @@
-public class BooleanResult {
-    private boolean success;
-
-    public BooleanResult(boolean v){
-        success = v;
-    }
-}
--- Java_server/src/main/java/LoginResult.java
+++
@@ -1,8 +0,0 @@
-public class LoginResult extends BooleanResult {
-    String sessionId;
-
-    public LoginResult(boolean r, String sessionId){
-        super(r);
-        this.sessionId = sessionId;
-    }
-}
