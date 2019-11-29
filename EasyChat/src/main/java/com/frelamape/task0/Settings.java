70f7697a9d9643fc366cdb83b70d9349b201b4ac
Riccardo Mancini <r.mancini@santannapisa.it>
2019-11-29 12:20:15 +0100

    Optimized imports

--- EasyChat/src/main/java/com/frelamape/task0/Settings.java
+++ EasyChat/src/main/java/com/frelamape/task0/Settings.java
@@ -1,7 +1,6 @@
 package com.frelamape.task0;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
